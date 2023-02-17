8cfb67823a95452ca496a3cc2b0378ffed6677c2
Caitriona71 <caitrionaboyle@ymail.com>
2023-02-17 22:56:15 +0000

    Import reverse and slugify to models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
